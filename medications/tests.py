9bab293e31ce9c75309a85dd30f78f0e508cf7e3
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-10-07 16:49:31 +0100

    deleted redundant test folder and used django inbuilt test file

--- medications/tests.py
+++ medications/tests.py
@@ -1,3 +1,29 @@
+"""Test Module for medication model"""
 from django.test import TestCase
+from .models import Medication
+from users.models import User
 
 # Create your tests here.
+
+class MedicationModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username="testuser")
+        self.medication = Medication.objects.create(
+            user=self.user,
+            name="Test Drug",
+            total_quantity=20,
+            dosage_per_intake=2,
+            frequency_per_day=3,
+            time_interval=8,
+        )
+
+    def test_take_dose_reduces_total_left(self):
+        self.assertEqual(self.medication.total_left, 20)
+        self.medication.take_dose()
+        self.assertEqual(self.medication.total_left, 18)
+
+    def test_medication_exhausted(self):
+        self.medication.total_left = 2
+        self.medication.take_dose()
+        self.assertEqual(self.medication.total_left, 0)
+        self.assertTrue(self.medication.is_exhausted())
