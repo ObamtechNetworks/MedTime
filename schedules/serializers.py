51aaab860e769f3f776b052b4abe243ac182f4c0
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-10-22 03:54:06 +0100

    added medication name via source(medication.drug_name) to schedules serialize
    this would allow the ability to get the related med & schedules from drugs data

--- schedules/serializers.py
+++ schedules/serializers.py
@@ -2,7 +2,9 @@ from rest_framework import serializers
 from .models import Schedule
 
 class ScheduleSerializer(serializers.ModelSerializer):
+    medication_name = serializers.CharField(source='medication.drug_name', read_only=True)  # Get the name of the medication
+
     class Meta:
         model = Schedule
-        fields = '__all__'
-        read_only_fields = ['id', 'created_at', 'updated_at']
+        fields = ['id', 'medication_name', 'created_at', 'next_dose_due_at', 'status']  # Only include the relevant fields
+        read_only_fields = ['id', 'created_at', 'updated_at']  # Ensure fields are read-only where necessary
